9ada0d6b8636a14de7588ca2abff7911f8f5cce5
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-03-28 20:37:48 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -27,16 +27,69 @@ loop();
 function keyDown( event )
 {
     var code = event.keyCode;
+    
+    if( code == 37 )
+    {
+        Player.velX = 2;
+    } else if( code == 38 )
+    {
+        Player.jump_bol = true;
+    } else if( code == 39 )
+    {
+        Player.velX = -2;
+    }
 }
 
 function keyUp( event )
 {
     var code = event.keyCode;
+    
+    if( code == 37 )
+    {
+        Player.velX = 0;
+    } else if( code == 38 )
+    {
+        Player.jump_bol = false;
+    } else if( code == 39 )
+    {
+        Player.velX = 0;
+    }
 }
+
+// mechanics functions
 
 function moviment()
 {
-    
+    Player.x += Player.velX;
+    Player.y += Player.jump;
+}
+
+function jump()
+{
+    if( Player.jump_bol != true && Player.gravity != false )
+    {
+        Player.jump_bol = true;
+        Player.gravity = false;
+        
+        if( Player.jump < 10 )
+        {
+            Player.jump += 1;
+        } 
+        else if( Player.jump >= 10 )
+        {
+            Player.jump_bol = false;
+            Player.jump = 0;
+            Player.gravity = true;
+        }
+    }
+}
+
+function gravity()
+{
+    if( Player.gravity == true )
+    {
+        Player.y += 1;
+    } 
 }
 
 // functions basic
@@ -46,6 +99,8 @@ function render()
     var img = new Image();
     img.src = "https://i.imgur.com/dUHek7i.png";
     ctx.drawImage(img, 0,0, canvas.width, canvas.height);
+    
+    ctx.drawImage(Player.image, Player.x, Player.y, 100,200);
 }
 
 function loop()
@@ -53,4 +108,6 @@ function loop()
     requestAnimationFrame(loop, canvas);
     moviment();
     render();
+    jump();
+    gravity();
 }
