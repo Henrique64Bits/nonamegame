1951b48c5195bd0f071dbf84daac84689004f7e4
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-03-31 20:22:27 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,52 @@ canvas.width = valor1;
 canvas.height = valor2;
 
 
+var viewport = {
+     screen      : [0,0],
+     startTile   : [0,0],
+     endTile     : [0,0],
+     offset      : [0,0],
+     update      : function( px, py )
+     {
+          this.offset[0] = Math.floor((this.screen[0]/2) - px);
+          this.offset[1] = Math.floor((this.screen[1]/2) - py);
+          
+          var tile = [
+               Math.floor(px/tileW),
+               Math.floor(py/tileH)
+          ];
+          
+          this.startTile[0] = tile[0] - 1 -
+               Math.ceil((this.screen[0]/2) / tileW);
+          this.startTile[1] = tile[1] - 1 -
+               Math.ceil((this.screen[1]/2) / tileH);
+          
+          if( this.startTile[0] < 0 )
+          {
+               this.startTile[0] = 0;
+          }
+          if( this.startTile[1] < 0 ) 
+          {
+               this.startTile[1] = 0;
+          }
+          
+          this.endTile[0] = tile[0] + 1 +
+               Math.ceil((this.screen[0]/2) / tileW);
+          this.endTile[1] = tile[1] + 1 +
+               Math.ceil((this.screen[1]/2) / tileH);
+               
+          if( this.endTile[0] >= mapW )
+          {
+               this.endTile[0] = mapW - 1;
+          }
+          if( this.endTile[1] >= mapH )
+          {
+               this.endTile[1] = mapH - 1;
+          }
+     }
+}
+
+
 
 for( a in _tiles )
 {
