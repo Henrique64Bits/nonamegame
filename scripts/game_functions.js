f1ffc1c35b563aff730b8e3ab0e4026a127e44bd
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-03-31 19:04:47 -0300

    Update game_functions.js

--- scripts/game_functions.js
+++ scripts/game_functions.js
@@ -30,3 +30,21 @@ function gravity()
         Player.y += 20;
     }
 }
+
+function colliders()
+{
+    for( a in Map.tiles_x )
+    {
+        for( i in Map.tiles_y )
+        {  
+            if( Map.tiles[i][a] != 0 )
+            {
+                if( Player.y + Player.height  > Map.tiles_y[i] && Player.y < Map.tiles_y[i]+35  &&  Player.x + Player.width  >= Map.tiles_x[a]  &&  Player.x <  Map.tiles_x[a] + 35 )
+                {
+                    Player.y = ( (Map.tiles_y[i] - Player.height) );
+                    Player.ground = true;
+                }
+            }
+        }
+    }
+}
