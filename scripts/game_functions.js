1169c834a8a8eb2d9fecf2322c1d3a5fc16e7ffb
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-03-31 19:03:20 -0300

    Update game_functions.js

--- scripts/game_functions.js
+++ scripts/game_functions.js
@@ -1,3 +1,32 @@
-function main()
+function moviment()
 {
+    Player.x += Player.velX;
+    Player.y = Player.y+Player.jump;
+}
+
+function jump()
+{
+    if( Player.jump_bol == true && Player.ground != false )
+    {   
+        if( Player.jump > -50 )
+        {
+            Player.gravity = false;
+            Player.jump += -10;
+        }
+        if( Player.jump <= -50 )
+        {
+            Player.jump_bol = false;
+            Player.jump = 0;
+            Player.gravity = true;
+        }
+    }
+}
+
+
+function gravity()
+{
+    if( Player.gravity == true )
+    {
+        Player.y += 20;
+    }
 }
