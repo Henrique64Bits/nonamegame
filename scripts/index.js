54d2c4d1aa18927df290c78d9bf1942e9ca5d8d3
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-03-31 22:08:48 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -11,54 +11,6 @@ canvas.width = valor1;
 canvas.height = valor2;
 
 
-var viewport = {
-     screen      : [0,0],
-     startTile   : [0,0],
-     endTile     : [0,0],
-     offset      : [0,0],
-     update      : function( px, py )
-     {
-          this.offset[0] = Math.floor((this.screen[0]/2) - px);
-          this.offset[1] = Math.floor((this.screen[1]/2) - py);
-          
-          var tile = [
-               Math.floor(px/35),
-               Math.floor(py/35)
-          ];
-          
-          this.startTile[0] = tile[0] - 1 -
-               Math.ceil((this.screen[0]/2) / 35);
-          this.startTile[1] = tile[1] - 1 -
-               Math.ceil((this.screen[1]/2) / 35);
-          
-          if( this.startTile[0] < 0 )
-          {
-               this.startTile[0] = 0;
-          }
-          if( this.startTile[1] < 0 ) 
-          {
-               this.startTile[1] = 0;
-          }
-          
-          this.endTile[0] = tile[0] + 1 +
-               Math.ceil((this.screen[0]/2) / 35);
-          this.endTile[1] = tile[1] + 1 +
-               Math.ceil((this.screen[1]/2) / 35);
-               
-          if( this.endTile[0] >= Map.mapW )
-          {
-               this.endTile[0] = Map.mapW - 1;
-          }
-          if( this.endTile[1] >= Map.mapH )
-          {
-               this.endTile[1] = Map.mapH - 1;
-          }
-     }
-}
-
-viewport.screen = [canvas.width, canvas.height];
-
-
 for( a in _tiles )
 {
      for( i in _tiles[a] )
@@ -101,10 +53,6 @@ function render()
                Map.tiles_x[i] = Map.tilesW*i;
                Map.tiles_y[a] = Map.tilesH*a;
                  
-               if( Map.tiles[a][i] == 0 )
-               {
-                    //none
-               }
                if( Map.tiles[a][i] == 1 )
                {
                    tm[a][i].src = "res/tile_1.png";
@@ -135,17 +83,4 @@ function loop()
     }
     
     colliders();
-}
-
-function clamp( value, min, max )
-{
-    if( value < min ) 
-    {
-        return min;
-    }
-    else if ( value > max ) 
-    {
-        return max;
-    }
-    return value;
 }
