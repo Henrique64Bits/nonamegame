a15776db908100672c3ded91acaa542cdd0d186b
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-01 23:06:53 +0000

    Update menu.js

--- scenes/menu.js
+++ scenes/menu.js
@@ -1,5 +1,13 @@
-var Menu = {};
-
-Menu.main = function()
-{
-}
+var Menu = {
+        background : new Image(),
+        main : function( canvas, ctx )
+        {
+                Menu.background.src = "res/background_0.png";
+                ctx.drawImage(Menu.background, 0,0, canvas.width, canvas.height);
+                
+                Menu.title = new Image();
+                Menu.title.src = "res/title_0.png";
+                ctx.drawImage(Menu.title, canvas.width/3, 50, 200, 70);
+                
+        }
+};
