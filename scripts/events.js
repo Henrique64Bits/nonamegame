2fd13b615057eda61a713aa7c18a9f3e6f9726bc
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-01 15:03:12 +0000

    Update events.js

--- scripts/events.js
+++ scripts/events.js
@@ -5,10 +5,11 @@ function keyDown( event )
     if( code == 37 )
     {
         Player.velX = -10;
-        Player.running = true;
+        Player.image.src = "res/bob/bob_0.gif";
     } else if( code == 39 )
     {
         Player.velX = 10;
+        Player.image.src = "res/bob/bob_1.gif";
     }
     if( code == 38 )
     {
@@ -23,10 +24,11 @@ function keyUp( event )
     if( code == 37 )
     {
         Player.velX = 0;
-        Player.running = false;
+        Player.image.src = "res/bob/bob_0.png";
     } else if( code == 39 )
     {
         Player.velX = 0;
+        Player.image.src = "res/bob/bob_1.png";
     }
     if( code == 38 )
     {
