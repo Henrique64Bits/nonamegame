2c6d3a99bdac180a15f956ed767d5e0bd490af48
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-02 16:12:49 +0000

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -1,9 +1,38 @@
 var Player = {
         position : [0,0],
         dims : [70,100],
+        vel : [0,0],
+        gravity : true,
+        jump : false,
+        ground : false,
         sprite : new Image(),
-        src : [["res/bob/bob_1-2.png", "res/bob/bob_1.png", "res/bob/bob_1-1.png"], ["res/bob/bob_0-2.png", "res/bob/bob_0.png", "res/bob/bob_0-1.png"]],
+        src : [["res/bob/bob_1-2.png", "res/bob/bob_1.png", "res/bob/bob_1-1.png"], 
+               ["res/bob/bob_0-2.png", "res/bob/bob_0.png", "res/bob/bob_0-1.png"]],
         src_count : [1,1],
+        
+        moviment : function()
+        {
+                Player.position[0] += Player.vel[0];
+                Player.position[1] += Player.vel[1];
+        },
+        
+        jump : function()
+        {
+                if( Player.jump == true )
+                {
+                        if( Player.vel[1] > -50 )
+                        {
+                                Player.gravity = false;
+                                Player.vel[1] += -10;
+                        }
+                        if( Player.vel[1] <= -50 )
+                        {
+                                Player.jump = false;
+                                Player.vel[1] = 0;
+                                Player.gravity = true;
+                        }
+                }
+        },
         
         update : function( canvas, ctx )
         {
