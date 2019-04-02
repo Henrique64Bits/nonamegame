5fa32ea89aace74169b13fd0652b8b62b5db83ab
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-02 01:16:26 +0000

    Update level_1.js

--- scenes/level_1.js
+++ scenes/level_1.js
@@ -3,11 +3,37 @@ var Level_1 = {
                 background : new Image(),
                 x : 0, y : 0,
                 width : 900, height : 200,
-                tiles : []
+                tiles : [],
+                clouds : {
+                        sprite : [],
+                        position : [],
+                        dims : [195, 87]
+                }
         },
         main : function( canvas, ctx )
         {
                 Level_1.map.background.src = "res/background_1.png";
                 ctx.drawImage(Level_1.map.background, 0,0, canvas.width, canvas.height);
+                
+                var _clouds = 4;
+                
+                for( a = 0; a < _clouds; a++ )
+                {
+                        Level_1.map.clouds.sprite[a] = new Image();
+                        Level_1.map.clouds.sprite[a].src = "res/cloud_1.png";
+                        Level_1.map.clouds.position[a] = [160+(230*a), 150];
+                }
+        },
+        loop : function( canvas, ctx )
+        {
+                for( a in Level_1.map.clouds.position )
+                {
+                        Level_1.map.clouds.position[a][0] += -3;
+                        
+                        if( Level_1.map.clouds.position[a][0] <= -190 )
+                        {
+                                Level_1.map.clouds.position[a][0] = (Level_1.map.clouds.position[a][0]*3+canvas.width) + (canvas.width+200);
+                        }
+                }
         }
 }
