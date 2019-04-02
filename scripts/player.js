7885bf1a2739789689059b44a0e1d7a602d4a9fd
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-02 14:48:09 +0000

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -2,10 +2,12 @@ var Player = {
         position : [0,0],
         dims : [70,100],
         sprite : new Image(),
+        src : [["res/bob/bob_1-2.png", "res/bob/bob_1.png", "res/bob/bob_1-1.png"], ["res/bob/bob_0-2.png", "res/bob/bob_0.png", "res/bob/bob_0-1.png"]],
+        src_count : [1,1],
         
         update : function( canvas, ctx )
         {
-                Player.sprite.src = "res/bob/bob_0.png";
+                Player.sprite.src = Player.src[Player.src_count[0]][Player.src_count[1]];
                 ctx.drawImage(Player.sprite, Player.position[0], Player.position[1], Player.dims[0], Player.dims[1]);
         }
 }
