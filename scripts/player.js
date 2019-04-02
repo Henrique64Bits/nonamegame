0a4dfd84ccaa9c3c617eab7274b3d9163b32bfc3
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-02 01:38:33 +0000

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -1,2 +1,11 @@
 var Player = {
+        position : [0,0],
+        dims : [70,100],
+        sprite : new Image(),
+        
+        update : function( canvas, ctx )
+        {
+                Player.sprite.src = "res/bob/bob_0.png";
+                ctx.drawImage(Player.sprite, Player.position[0], Player.position[1], Player.dims[0], Player.dims[1]);
+        }
 }
