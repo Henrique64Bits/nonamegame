12aebf1f431d544a600de827adc238ea526b781b
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-02 00:55:17 +0000

    Update level_1.js

--- scenes/level_1.js
+++ scenes/level_1.js
@@ -1,2 +1,13 @@
-var Game = {
+var Level_1 = {
+        map : {
+                background : new Image(),
+                x : 0, y : 0,
+                width : 900, height : 200,
+                tiles : []
+        },
+        main : function( canvas, ctx )
+        {
+                Level_1.map.background.src = "res/background_1.png";
+                ctx.drawImage(Level_1.map.background, 0,0, canvas.width, canvas.height);
+        }
 }
