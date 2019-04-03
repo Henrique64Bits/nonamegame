043d7ba8eea55fcb8b49956009066d3345a3f116
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-03 19:49:27 -0300

    Update events.js

--- scripts/events.js
+++ scripts/events.js
@@ -2,36 +2,12 @@ function keyDown( event )
 {
     var code = event.keyCode;
     
-    if( code == 37 )
-    {
-        Player.velX = -10;
-        Player.runLeft = true;
-    } else if( code == 39 )
-    {
-        Player.velX = 10;
-        Player.runRight = true;
-    }
-    if( code == 38 )
-    {
-        Player.jump_bol = true;
-    }
+    Level_1.keyDown(event);
 }
 
 function keyUp( event )
 {
     var code = event.keyCode;
     
-    if( code == 37 )
-    {
-        Player.velX = 0;
-        Player.runLeft = false;
-    } else if( code == 39 )
-    {
-        Player.velX = 0;
-        Player.runRight = false;
-    }
-    if( code == 38 )
-    {
-        // pass
-    }
+    Level_1.keyUp(event);
 }
