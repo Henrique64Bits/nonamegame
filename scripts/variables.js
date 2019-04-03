5c7a4015c943530e052b47c1d8e62822d934a2f8
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-03 19:47:40 -0300

    Update variables.js

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,24 +1,3 @@
-var Map = {
-                viewX           : 0,
-                viewY           : 0,
-                tiles           : [],
-                tilesW          : 35,
-                tilesH          : 35,
-                tiles_img       : ["","res/tiles_0.png"],
-                tiles_x         : [],
-                tiles_y         : [],
-                mapW            : 2000,
-                mapH            : 1000
-}
-
-var Tiles = {
-                tiles_x         : [],
-                tiles_y         : [],
-                tiles_img       : [],
-                tiles_w         : [],
-                tiles_h         : []
-}
-
 var Blocks = [
                 {
                                 sprite : "",
@@ -29,29 +8,3 @@ var Blocks = [
                                 dims : [30, 30]
                 }
 ];
-
-for( a in Tiles.tiles_img )
-{
-                Tiles.tiles_img[a] = [];
-                Tiles.tiles_x[a] = [];
-                Tiles.tiles_y[a] = [];
-                Tiles.tiles_w[a] = [];
-                Tiles.tiles_h[a] = [];
-}
-
-var Player = {
-                x: 100,
-                y: 70,
-                width: 30,
-                height: 70,
-                src: [["res/bob/bob_0-1.png","res/bob/bob_0.png","res/bob/bob_0-2"], ["res/bob/bob_1-1.png","res/bob/bob_1.png","res/bob/bob_1-2"]],
-                velX: 0,
-                jump: 0,
-                jump_bol: false,
-                gravity: true,
-                ground: false,
-                runLeft: false, runRight: false,
-                src_count: [0, 1]
-};
-Player.image = new Image();
-Player.image.src = Player.src[Player.src_count[0]][Player.src_count[1]];
