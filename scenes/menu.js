75b639d09966c3e9f9e99d5c318809fc5d6037ec
Henrique64Bits <48572224+Henrique64Bits@users.noreply.github.com>
2019-04-03 22:27:19 -0300

    Update menu.js

--- scenes/menu.js
+++ scenes/menu.js
@@ -62,9 +62,8 @@ var Menu = {
                 Menu.background.src = "res/background_1.png";
                 ctx.drawImage(Menu.background, 0,0, canvas.width, canvas.height);
                 
-                Menu.title = new Image();
-                Menu.title.src = "res/title_0.png";
-                ctx.drawImage(Menu.title, canvas.width/3, 50, 200, 70);
+                ctx.font = "40px Kiwi";
+                ctx.fillText("HeroBoy", canvas.width/3, 50);
                 
                 if( Menu.select == 0 )
                 {
